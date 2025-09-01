af1758831d328a2da4d12583582f3126c5f2ec3c
andyzhoulf-stack <andyzhoulf@gmail.com>
2025-09-02 04:41:14 +0800

    Update encrypt.h

--- encrypt.h
+++ encrypt.h
@@ -1,23 +1,37 @@
 #ifndef ENCRYPT_H
 #define ENCRYPT_H
 
-#include <cstring>
-#include <cstdio> // 新增，支持 printf/sprintf
-#include <cstdint> // 如果你用到了 uint32_t
-
-#pragma once
+#include <stdint.h>
 
 #ifdef _WIN32
-#define DLL_EXPORT extern "C" __declspec(dllexport)
+#define DLL_EXPORT __declspec(dllexport)
 #else
-#define DLL_EXPORT extern "C"
+#define DLL_EXPORT
 #endif
 
-// rotr32 实现
-inline uint32_t rotr32(uint32_t value, unsigned int count) {
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// 32位右旋工具
+static inline uint32_t rotr32(uint32_t value, unsigned int count) {
     return (value >> count) | (value << (32 - count));
 }
 
-DLL_EXPORT void encrypt_1(char* imei, char* resbuf, int version);
+// 出口1：算e630upgrade版本的key
+DLL_EXPORT void calc1_flash(const char* imei, char* outbuf);
+
+// 出口2：算hwe620datacard版本的key
+DLL_EXPORT void calc1(const char* imei, char* outbuf);
+
+// 出口3：V2算法
+DLL_EXPORT void calc2(const char* imei, char* outbuf);
+
+// 出口4：V201算法
+DLL_EXPORT void calc201(const char* imei, char* outbuf);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // ENCRYPT_H
