fd2bdc9908ee8c36be3b6a038b10e7d896f7f84a
andyzhoulf-stack <andyzhoulf@gmail.com>
2025-09-02 01:32:21 +0800

    Update encrypt.h

--- encrypt.h
+++ encrypt.h
@@ -1,7 +1,10 @@
 #ifndef ENCRYPT_H
 #define ENCRYPT_H
 
-#include <cstring> // C++风格
+#include <cstring>
+#include <cstdio> // 新增，支持 printf/sprintf
+#include <cstdint> // 如果你用到了 uint32_t
+
 #pragma once
 
 #ifdef _WIN32
@@ -9,6 +12,11 @@
 #else
 #define DLL_EXPORT extern "C"
 #endif
+
+// rotr32 实现
+inline uint32_t rotr32(uint32_t value, unsigned int count) {
+    return (value >> count) | (value << (32 - count));
+}
 
 DLL_EXPORT void encrypt_1(char* imei, char* resbuf, int version);
 
