676d1f38fa9fa4ba87a03a70217b303a538140e8
andyzhoulf-stack <andyzhoulf@gmail.com>
2025-09-02 12:08:28 +0800

    Update encrypt.h

--- encrypt.h
+++ encrypt.h
@@ -1,37 +1,23 @@
-#ifndef ENCRYPT_H
-#define ENCRYPT_H
+//
+//  Калькулятор NCK-кодов для модемов Huawei с новым алгоритмом v201
+//
+//  Автор - forth32  
+//  2014 год
+//
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-#include <cstdint> // for uint32_t
+int calc201(char* imei, char* resbuf);
+int calc2(char* imei, char* resbuf);
+void encrypt_1(char* imei,char* resbuf,int version);
+void encrypt_2(char* imei,char* resbuf,int version);
+void encrypt_3(char* imei,char* resbuf,int version);
+void encrypt_4(char* imei,char* resbuf,int version);
+void encrypt_5_v2(char* imei,char* resbuf);
+void encrypt_6(char* imei,char* resbuf,int mode);
+void encrypt_7(char* imei,char* resbuf,int version);
 
-#ifdef _WIN32
-#define DLL_EXPORT __declspec(dllexport)
-#else
-#define DLL_EXPORT
-#endif
+void encrypt_v1(char* imei,char* resbuf,char* pass);
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-// 32位右旋工具
-static inline uint32_t rotr32(uint32_t value, unsigned int count) {
-    return (value >> count) | (value << (32 - count));
-}
-
-// 出口1：算e630upgrade版本的key
-DLL_EXPORT void calc1_flash(const char* imei, char* outbuf);
-
-// 出口2：算hwe620datacard版本的key
-DLL_EXPORT void calc1(const char* imei, char* outbuf);
-
-// 出口3：V2算法
-DLL_EXPORT void calc2(const char* imei, char* outbuf);
-
-// 出口4：V201算法
-DLL_EXPORT void calc201(const char* imei, char* outbuf);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif // ENCRYPT_H
+unsigned int  rotr32(unsigned int val, int n);
