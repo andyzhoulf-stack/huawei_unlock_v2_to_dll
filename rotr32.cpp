284e421cd57df69d956090f98f4482c3c1310883
andyzhoulf-stack <andyzhoulf@gmail.com>
2025-09-02 14:20:47 +0800

    Update rotr32.cpp

--- rotr32.cpp
+++ rotr32.cpp
@@ -1,4 +1,6 @@
 #include "encrypt.h"
+#include <stdint.h>
+// 不要再加extern "C"，头文件已经包裹了
 
 uint32_t rotr32(uint32_t value, unsigned int count) {
     return (value >> count) | (value << (32 - count));
