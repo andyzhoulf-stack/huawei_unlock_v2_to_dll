b7e4bd28b7a481bd71335a230dc1664e51c4b8cd
andyzhoulf-stack <andyzhoulf@gmail.com>
2025-09-02 14:06:39 +0800

    Update encrypt.h

--- encrypt.h
+++ encrypt.h
@@ -1,5 +1,6 @@
 #ifndef ENCRYPT_H
 #define ENCRYPT_H
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -19,7 +20,9 @@
 #  endif
 #endif
 
-DLL_EXPORT uint32_t rotr32(uint32_t value, unsigned int count);
+// rotr32一般做内部工具函数，不导出；如果你需要导出，则加DLL_EXPORT
+// uint32_t rotr32(uint32_t value, unsigned int count);
+uint32_t rotr32(uint32_t value, unsigned int count); // 内部函数，不导出
 
 DLL_EXPORT int calc201(char* imei, char* resbuf);
 DLL_EXPORT int calc2(char* imei, char* resbuf);
