1c645bab6d8b4d3321d57582c0ffbaaa42daefb0
andyzhoulf-stack <andyzhoulf@gmail.com>
2025-09-02 12:33:02 +0800

    Update encrypt.h

--- encrypt.h
+++ encrypt.h
@@ -1,23 +1,30 @@
-//
-//  Калькулятор NCK-кодов для модемов Huawei с новым алгоритмом v201
-//
-//  Автор - forth32  
-//  2014 год
-//
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#ifndef ENCRYPT_H
+#define ENCRYPT_H
 
-int calc201(char* imei, char* resbuf);
-int calc2(char* imei, char* resbuf);
-void encrypt_1(char* imei,char* resbuf,int version);
-void encrypt_2(char* imei,char* resbuf,int version);
-void encrypt_3(char* imei,char* resbuf,int version);
-void encrypt_4(char* imei,char* resbuf,int version);
-void encrypt_5_v2(char* imei,char* resbuf);
-void encrypt_6(char* imei,char* resbuf,int mode);
-void encrypt_7(char* imei,char* resbuf,int version);
+#ifdef _WIN32
+#  ifdef __cplusplus
+#    define DLL_EXPORT extern "C" __declspec(dllexport)
+#  else
+#    define DLL_EXPORT __declspec(dllexport)
+#  endif
+#else
+#  ifdef __cplusplus
+#    define DLL_EXPORT extern "C"
+#  else
+#    define DLL_EXPORT
+#  endif
+#endif
 
-void encrypt_v1(char* imei,char* resbuf,char* pass);
+DLL_EXPORT int calc201(char* imei, char* resbuf);
+DLL_EXPORT int calc2(char* imei, char* resbuf);
+DLL_EXPORT void encrypt_1(char* imei, char* resbuf, int version);
+DLL_EXPORT void encrypt_2(char* imei, char* resbuf, int version);
+DLL_EXPORT void encrypt_3(char* imei, char* resbuf, int version);
+DLL_EXPORT void encrypt_4(char* imei, char* resbuf, int version);
+DLL_EXPORT void encrypt_5_v2(char* imei, char* resbuf);
+DLL_EXPORT void encrypt_6(char* imei, char* resbuf, int mode);
+DLL_EXPORT void encrypt_7(char* imei, char* resbuf, int version);
+DLL_EXPORT void encrypt_v1(char* imei, char* resbuf, char* pass);
+DLL_EXPORT unsigned int rotr32(unsigned int val, int n);
 
-unsigned int  rotr32(unsigned int val, int n);
+#endif // ENCRYPT_H
