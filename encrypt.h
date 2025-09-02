d68f4eaab7be79e3fa0ed32ffd3fa88193d6ebdc
andyzhoulf-stack <andyzhoulf@gmail.com>
2025-09-02 14:33:54 +0800

    Update encrypt.h

--- encrypt.h
+++ encrypt.h
@@ -13,16 +13,18 @@
 #    define DLL_EXPORT __declspec(dllexport)
 #  endif
 #else
+#  ifdef __cplusplus
+#    define DLL_EXPORT extern "C"
+#  else
+#    define DLL_EXPORT
+#  endif
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 uint32_t rotr32(uint32_t value, unsigned int count);
-
-#ifdef __cplusplus
-}
-#endif
-
 
 DLL_EXPORT int calc201(char* imei, char* resbuf);
 DLL_EXPORT int calc2(char* imei, char* resbuf);
@@ -34,5 +36,9 @@ DLL_EXPORT void encrypt_5_v2(char* imei, char* resbuf);
 DLL_EXPORT void encrypt_6(char* imei, char* resbuf, int mode);
 DLL_EXPORT void encrypt_7(char* imei, char* resbuf, int version);
 DLL_EXPORT void encrypt_v1(char* imei, char* resbuf, char* pass);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // ENCRYPT_H
