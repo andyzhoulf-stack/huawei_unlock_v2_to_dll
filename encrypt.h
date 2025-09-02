31c6da2697e50e2840e4541d29694f263221eb1b
andyzhoulf-stack <andyzhoulf@gmail.com>
2025-09-02 13:27:18 +0800

    Update encrypt.h

--- encrypt.h
+++ encrypt.h
@@ -1,8 +1,9 @@
+#ifndef ENCRYPT_H
+#define ENCRYPT_H
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#ifndef ENCRYPT_H
-#define ENCRYPT_H
+#include <stdint.h>
 
 #ifdef _WIN32
 #  ifdef __cplusplus
@@ -18,6 +19,8 @@
 #  endif
 #endif
 
+DLL_EXPORT uint32_t rotr32(uint32_t value, unsigned int count);
+
 DLL_EXPORT int calc201(char* imei, char* resbuf);
 DLL_EXPORT int calc2(char* imei, char* resbuf);
 DLL_EXPORT void encrypt_1(char* imei, char* resbuf, int version);
@@ -28,6 +31,5 @@ DLL_EXPORT void encrypt_5_v2(char* imei, char* resbuf);
 DLL_EXPORT void encrypt_6(char* imei, char* resbuf, int mode);
 DLL_EXPORT void encrypt_7(char* imei, char* resbuf, int version);
 DLL_EXPORT void encrypt_v1(char* imei, char* resbuf, char* pass);
-DLL_EXPORT unsigned int rotr32(unsigned int val, int n);
 
 #endif // ENCRYPT_H
